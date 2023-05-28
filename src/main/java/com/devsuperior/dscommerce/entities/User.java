7adc2fd45d6b985aa1061b9de57d5dd9913ccd1e
Michael Douglas <mikedgls17@gmail.com>
2023-05-27 22:11:32 -0300

    entidade User, banco de dados H2.

---
+++ src/main/java/com/devsuperior/dscommerce/entities/User.java
@@ -0,0 +1,28 @@
+package com.devsuperior.dscommerce.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+@Table(name = "tb_user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    private Long id;
+    private String name;
+    private String email;
+    private String phone;
+    private LocalDate birthDate;
+    private String password;
+
+}
